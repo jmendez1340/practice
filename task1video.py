de8b51de1a2697e82d76da65527ce9eb02bb5f8b
Jason Mendez <jasonmendez795@gmail.com>
2016-11-21 19:12:03 -0600

    Add files via upload

---
+++ task1video.py
@@ -0,0 +1,26 @@
+selection = 0
+
+while selection >= 0:
+
+    print "1.Returning Customer"
+    print "2.New Customer"
+    print "3.Guest"
+    print "======================\n"
+
+    selection = (raw_input("Please select your customer type:   "))
+    try:
+        if selection < 1 or selection > 3:
+            selection = int(raw_input("Please select your customer type\n Enter a number between 1 and 3   "))
+        elif selection == 1:
+            print "Returning Customer"
+            selection = -1
+        elif selection == 2:
+            print "New Customer"
+            selection = -1
+        elif selection == 3:
+            print "Guest"
+            selection = -1
+        else:
+            print "\n\n Please enter a number\n"
+    except ValueError:
+        print "Please enter a number"
